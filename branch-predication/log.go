c8dfa9e41808b44126aa21cee71edc3cad7bbf5a
Tao Wen <taowen@gmail.com>
2018-01-29 22:39:23 +0800

    add branch predication

---
+++ branch-predication/log.go
@@ -0,0 +1,15 @@
+package branch_predication
+
+import "fmt"
+
+const LevelTrace = 10
+const LevelDebug = 20
+
+var MinLevel = LevelDebug
+
+func Trace(event string) {
+	if LevelTrace < MinLevel {
+		return
+	}
+	fmt.Println(event)
+}
---
+++ branch-predication/log_test.go
@@ -0,0 +1,10 @@
+package branch_predication
+
+import "testing"
+
+func Benchmark_trace_branch_predication(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		Trace("hello")
+	}
+}
